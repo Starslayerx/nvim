93baec48b7a18c1cc9c4b8d3812ac40ca198b5ef
Starslayerx <1049353754@qq.com>
2025-10-11 13:51:17 +0800

    remove treesitter <CR> keybinds

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -66,11 +66,6 @@ return {
     lazy = false, -- 文档明确说明不支持 lazy-loading
     branch = "main",
     build = ":TSUpdate",
-    keys = {
-      { "<CR>", desc = "Treesitter: Init/Expand selection", mode = { "n", "x" } },
-      { "<BS>", desc = "Treesitter: Shrink selection", mode = "x" },
-      { "<TAB>", desc = "Treesitter: Scope incremental", mode = "x" },
-    },
     config = function()
       -- 基础配置
       require("nvim-treesitter").setup({
@@ -308,7 +303,47 @@ return {
         theme = "nord",
         component_separators = { left = "|", right = "|" },
         section_separators = { left = "", right = "" },
+        always_divide_middle = true,
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = {
+          "branch",
+          "diff",
+          {
+            "diagnostics",
+            sources = { "nvim_diagnostic" },
+            symbols = { error = " ", warn = " ", info = " ", hint = " " },
+          },
+        },
+        lualine_c = { "filename" },
+        lualine_x = { "copilot", "encoding", "fileformat", "filetype" }, -- I added copilot here
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      tabline = {},
+      extensions = {},
+      spinners = {
+        "✶",
+        "✸",
+        "✹",
+        "✺",
+        "✹",
+        "✷",
       },
     },
+  },
+
+  -- copilot with lualine
+  {
+    "AndreM222/copilot-lualine",
   },
 }
